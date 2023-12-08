748701952e0c7917dfce838d03f30c0184771a94
muhammadsampaga <60804326+muhammadsampaga@users.noreply.github.com>
2023-12-08 10:23:10 +0700

    CRUD-Siswa

---
+++ form-daftar.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulir Pendaftaran Siswa Baru | SMK Kalisarian</title>
+    <link rel="stylesheet" type="text/css" href="./css/daftar.css">
+    <script>
+        function validateForm() {
+            var nama = document.forms["daftarForm"]["nama"].value;
+            var alamat = document.forms["daftarForm"]["alamat"].value;
+            var jenisKelamin = document.forms["daftarForm"]["jenis_kelamin"].value;
+            var agama = document.forms["daftarForm"]["agama"].value;
+            var sekolahAsal = document.forms["daftarForm"]["sekolah_asal"].value;
+
+            if (nama == "" || alamat == "" || jenisKelamin == "" || agama == "" || sekolahAsal == "") {
+                alert("Semua kolom harus diisi!");
+                return false;
+            }
+        }
+    </script>
+</head>
+
+<body>
+    <header>
+        <h3>Formulir Pendaftaran Siswa Baru</h3>
+    </header>
+
+    <form action="proses-pendaftaran.php" method="POST" name="daftarForm" onsubmit="return validateForm()">
+
+        <fieldset>
+
+        <p>
+            <label for="nama">Nama: </label>
+            <input type="text" name="nama" placeholder="nama lengkap" />
+        </p>
+        <p>
+            <label for="alamat">Alamat: </label>
+            <textarea name="alamat"></textarea>
+        </p>
+        <p>
+            <label for="jenis_kelamin">Jenis Kelamin: </label>
+            <label><input type="radio" name="jenis_kelamin" value="laki-laki"> Laki-laki</label>
+            <label><input type="radio" name="jenis_kelamin" value="perempuan"> Perempuan</label>
+        </p>
+        <p>
+            <label for="agama">Agama: </label>
+            <select name="agama">
+                <option>Islam</option>
+                <option>Kristen</option>
+                <option>Hindu</option>
+                <option>Budha</option>
+                <option>Atheis</option>
+            </select>
+        </p>
+        <p>
+            <label for="sekolah_asal">Sekolah Asal: </label>
+            <input type="text" name="sekolah_asal" placeholder="nama sekolah" />
+        </p>
+        <p>
+            <input type="submit" value="Daftar" name="daftar" />
+        </p>
+
+        </fieldset>
+
+    </form>
+
+</body>
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- <!DOCTYPE html>
+<html>
+<head>
+    <title>Formulir Pendaftaran Siswa Baru | SMK Kalisarian</title>
+</head>
+
+<body>
+    <header>
+        <h3>Formulir Pendaftaran Siswa Baru</h3>
+    </header>
+
+    <form action="proses-pendaftaran.php" method="POST">
+
+        <fieldset>
+
+        <p>
+            <label for="nama">Nama: </label>
+            <input type="text" name="nama" placeholder="nama lengkap" />
+        </p>
+        <p>
+            <label for="alamat">Alamat: </label>
+            <textarea name="alamat"></textarea>
+        </p>
+        <p>
+            <label for="jenis_kelamin">Jenis Kelamin: </label>
+            <label><input type="radio" name="jenis_kelamin" value="laki-laki"> Laki-laki</label>
+            <label><input type="radio" name="jenis_kelamin" value="perempuan"> Perempuan</label>
+        </p>
+        <p>
+            <label for="agama">Agama: </label>
+            <select name="agama">
+                <option>Islam</option>
+                <option>Kristen</option>
+                <option>Hindu</option>
+                <option>Budha</option>
+                <option>Atheis</option>
+            </select>
+        </p>
+        <p>
+            <label for="sekolah_asal">Sekolah Asal: </label>
+            <input type="text" name="sekolah_asal" placeholder="nama sekolah" />
+        </p>
+        <p>
+            <input type="submit" value="Daftar" name="daftar" />
+        </p>
+
+        </fieldset>
+
+    </form>
+
+    </body>
+</html> -->
---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Pendaftaran Siswa Baru | SMK Kalisarian</title>
+    <link rel="stylesheet" type="text/css" href="./css/indexcss.css">
+</head>
+
+<body>
+    <header>
+        <h3>Pendaftaran Siswa Baru</h3>
+        <h1>SMK Kalisarian</h1>
+    </header>
+
+    <h4>Menu</h4>
+    <nav>
+        <ul>
+            <li><a href="form-daftar.php">Daftar Baru</a></li>
+            <li><a href="list-siswa.php">Pendaftar</a></li>
+        </ul>
+    </nav>
+</body>
+
+</html>
